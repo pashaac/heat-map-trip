1ef241a435d51970540f49d94969081ca9fee628
Pavel Asadchiy <pavel.asadchiy@gmail.com>
2018-05-11 01:01:20 +0300

    Last fixes

--- src/main/resources/static/venue-interface.js
+++ src/main/resources/static/venue-interface.js
@@ -1,6 +1,44 @@
 var venueMarkers = [];
 var venues = [];
 
+function googleMapShowVenuesButtonInitialization2() {
+    $("#map-show-venues-button").click(function () {
+        if (!isValidEnvironment()) {
+            return;
+        }
+
+        var city = JSON.parse(sessionStorage.getItem(MAP_CITY_KEY));
+        var categories = $("#google-map-venue-category").val();
+        var source = $("#google-map-venue-source").val();
+        var grid = $("#google-map-grid-slider").val();
+
+        var params = jQuery.param({
+            cityId: city.id,
+            source: source.toUpperCase(),
+            grid: grid,
+            categories: categories.join(',')
+        });
+        // $.put("/venues/collection?" + params, function (_venues) {
+        $.get("http://localhost:8080" + "/boundingboxes/grid/collection?" + params, function (_venues) {
+            // var venuesIds = _venues.map(function (venue) {
+            //     return venue.id;
+            // });
+            // $.put("/venues/validation", JSON.stringify(venuesIds), function (validVenues) {
+            _venues.forEach(function (venue) {
+                venueMarkers.push(googleMarker(venue));
+            });
+            venues = venues.concat(_venues);
+            $("#google-map-heat-map-slider").prop("disabled", false);
+            $("#google-map-grid-heat-map-button").prop("disabled", false);
+            showInvalidBoundingBoxes(city, categories, source);
+            // }, function () {
+            //     console.error("Heat-map venues validation service temporary unavailable...");
+            //     alert("Heat-map venues validation service temporary unavailable...\nRepeat your last act after some pause or contact with developer");
+            // });
+        });
+    })
+}
+
 function googleMapShowVenuesButtonInitialization() {
     $("#map-show-venues-button").click(function () {
         if (!isValidEnvironment()) {
@@ -26,9 +64,6 @@ function googleMapShowVenuesButtonInitialization() {
                 console.error("Heat-map venues validation service temporary unavailable...");
                 alert("Heat-map venues validation service temporary unavailable...\nRepeat your last act after some pause or contact with developer");
             });
-        }, function () {
-            console.error("Heat-map venues mine service temporary unavailable...");
-            alert("Heat-map venues mine service temporary unavailable...\nRepeat your last act after some pause or contact with developer");
         });
     })
 }
